7ae1cc3010d06771c4eb514e1f200fc580410d68
Jagdish Singh <jagdish.chauhan01@gmail.com>
2018-10-09 16:38:08 +0530

    macro created

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,51 @@
 $(function () {
     var client = ZAFClient.init();
-    client.invoke('resize', { width: '100%', height: '100%' });
+    client.invoke('resize', {
+        width: '100%',
+        height: '100%'
+    });
 
+    var url = window.location.href;
+    url = url.split('=');
+    url = url[1].split('&');
+    var base_url = decodeURIComponent(url[0]);
+
+    $(document).on('click', '#submit', function () {
+        let title = $('#macro').val();
+        let comment = $('#macroDesc').val();
+        if (title === "" || comment === "")
+            return;
+        let data = {
+            macro: {
+                'title': title,
+                'actions': [{
+                    'field': 'comment_value',
+                    'value': comment
+                }]
+            }
+        };
+        var options = {
+            url: base_url + "/api/v2/macros.json",
+            type: 'POST',
+            data: JSON.stringify(data),
+            contentType: "application/json",
+            cors: true
+        };
+        client.request(options).then(
+            function (response) {
+                $('#macro').val('');
+                $('#macroDesc').val('');
+                $('#msg').append(
+                    '<div class="alert alert-success alert-dismissible fade show">' +
+                    '<button type="button" class="close" data-dismiss="alert">&times;</button>' +
+                    '<strong>Success!</strong> Macro Created.' +
+                    '</div>'
+                );
+
+                setTimeout(function(){
+                    $('.alert').hide(500);
+                },1000);
+            });
+
+    });
 });

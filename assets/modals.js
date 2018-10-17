7f2a20d4c895733f83c9b87ee2d9ae4838900a10
Jagdish Singh <jagdish.chauhan01@gmail.com>
2018-10-17 15:04:50 +0530

    pagination

--- assets/modals.js
+++ assets/modals.js
@@ -1,10 +1,13 @@
 var srcType = 'all',
     pc;
+
 var url = window.location.href;
 url = url.split('=');
 url = url[1].split('&');
 var base_url = decodeURIComponent(url[0]);
+
 var getArticles;
+var appendArticles;
 
 var typingTimer;
 var doneTyping;
@@ -32,9 +35,48 @@ $(function () {
 
     //actual modal functionality
 
-    getArticles = function (data) {
+    appendArticles = function (response) {
+        for (let i = 0; i < response.count; i++) {
+            let j = i + 1;
+
+            let url = response.results["0"].html_url;
+            let title = response.results[i].title;
+            let body = response.results[i].body;
+
+            let article = '<div class="card">' +
+                '<div class="card-header row">' +
+                '<div class="col-sm-10">' +
+                '<div class="collapsed" id="heading' + j + '" data-toggle="collapse" data-target="#collapse' + j +
+                '" aria-expanded="false" aria-controls="collapse"' + j + '">' +
+                '<div class="row">' +
+                '<div class="col-sm-2">#' + j + '</div>' +
+                '<div class="col-sm-10">' +
+                '<div class="postHeading" id="article_' + j + '">' + title + '</div>' +
+                '</div>' +
+                '</div>' +
+                '</div>' +
+                '</div>' +
+                '<div class="col-sm-2">' +
+                '<button type="button" class="btn copy-btn copy-link" id="copy-link-' + j + '">&#x1F517;</button>' +
+                '<div class="float-right">' +
+                '<button type="button" class="btn copy-btn copy-text" id="copy-txt-' + j + '">Copy</button>' +
+                '</div>' +
+                '</div>' +
+                '</div>' +
+                '<div id="collapse' + j + '" class="collapse" aria-labelledby="heading' + j + '" data-parent="#accordion">' +
+                '<a class="article-link" id="article_link_' + j + '" id="article_link_' + j + '" href="' + url + '"></a>' +
+                '<div class="card-body" id="article_body_' + j + '">' +
+                body +
+                '</div>' +
+                '</div>' +
+                '</div>';
+            $('#accordion').append(article);
+        }
+    }
+
+    getArticles = function (url, isAppend) {
         var options = {
-            url: base_url + "/api/v2/help_center/articles/search.json?query=" + data,
+            url: url,
             type: 'GET',
             contentType: "application/json",
             cors: true
@@ -42,42 +84,15 @@ $(function () {
         pc.request(options).then(
             function (response) {
                 console.log(response);
-                $('#accordion').empty();
-                for (let i = 0; i < response.count; i++) {
-                    let j = i + 1;
-
-                    let url = response.results["0"].html_url;
-                    let title = response.results[i].title;
-                    let body = response.results[i].body;
-
-                    let article = '<div class="card">' +
-                        '<div class="card-header row">' +
-                        '<div class="col-sm-10">' +
-                        '<div class="collapsed" id="heading' + j + '" data-toggle="collapse" data-target="#collapse' + j +
-                        '" aria-expanded="false" aria-controls="collapse"' + j + '">' +
-                        '<div class="row">' +
-                        '<div class="col-sm-2">#' + j + '</div>' +
-                        '<div class="col-sm-10">' +
-                        '<div class="postHeading" id="article_' + j + '">' + title + '</div>' +
-                        '</div>' +
-                        '</div>' +
-                        '</div>' +
-                        '</div>' +
-                        '<div class="col-sm-2">' +
-                        '<button type="button" class="btn copy-btn copy-link" id="copy-link-' + j + '">&#x1F517;</button>' +
-                        '<div class="float-right">' +
-                        '<button type="button" class="btn copy-btn copy-text" id="copy-txt-' + j + '">Copy</button>' +
-                        '</div>' +
-                        '</div>' +
-                        '</div>' +
-                        '<div id="collapse' + j + '" class="collapse" aria-labelledby="heading' + j + '" data-parent="#accordion">' +
-                        '<a class="article-link" id="article_link_' + j + '" id="article_link_' + j + '" href="' + url + '"></a>' +
-                        '<div class="card-body" id="article_body_' + j + '">' +
-                        body +
-                        '</div>' +
-                        '</div>' +
-                        '</div>';
-                    $('#accordion').append(article);
+                if(response.page === 1){
+                    $('#accordion').empty();
+                    if(response.count === 0){
+                        $('#accordion').append('<div> No data found...</div>');
+                    }
+                }
+                appendArticles(response);
+                while(response.next_page){
+                    getArticles(response.next_page, false);
                 }
             });
     };
@@ -85,7 +100,8 @@ $(function () {
     doneTyping = function () {
         let data = $('#search').val();
         if (data) {
-            getArticles(data);
+            data = base_url + "/api/v2/help_center/articles/search.json?query=" + data
+            getArticles(data, false);
         }
     }
 
